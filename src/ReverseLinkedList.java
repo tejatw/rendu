e63fd56d19763e0dd286307e98d24f96220fe95a
lo13 <lo13@global.tesco.org>
2018-11-26 22:50:27 +0530

    * master, reverse linked list

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,10 +1,84 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 public class ReverseLinkedList {
 
-    @Data
-    public class Node{
 
-        private int data;
+    @Data
+    @AllArgsConstructor
+    @RequiredArgsConstructor
+    public static class Node {
+
+        private Integer data;
         private Node next;
 
+        public Node(Integer data) {
+
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static void main(String... args) {
+
+        Node n4 = new Node(4);
+        Node n3 = new Node(3, n4);
+        Node n2 = new Node(2, n3);
+        Node n1 = new Node(1, n2);
+
+        Node head  = new Node();
+        head = n1;
+
+        printLinkedList(head);
+
+        Node tail = new Node();
+
+        tail = findLinkedListTail(head);
+
+        head = reverseLinkedList(head);
+
+        printLinkedList(tail);
+
+
+    }
+
+    public static void printLinkedList(Node head) {
+
+        Node n = head;
+
+        while (n != null) {
+
+            System.out.println(n.getData());
+            n = n.getNext();
+        }
+    }
+
+    public static Node reverseLinkedList(Node head) {
+
+        if (head.getNext() == null) {
+
+            return head;
+        }
+
+        Node temp = reverseLinkedList(head.getNext());
+        temp.setNext(head);
+        head.setNext(null);
+
+        return head;
+
+
+    }
+
+    public static Node findLinkedListTail(Node head) {
+
+        while (head.getNext() != null) {
+
+            head = head.getNext();
+        }
+
+        return head;
     }
 }
