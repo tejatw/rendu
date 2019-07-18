9e52a03f1236cfc1e5422dd5946e5d0066e435fe
lo13 <lo13@global.tesco.org>
2019-07-18 22:24:34 +0530

    master, hackerrank warmupchallenge 2

--- src/hackerrank/warmup/RepeatedString.java
+++ src/hackerrank/warmup/RepeatedString.java
@@ -1,4 +1,80 @@
 package hackerrank.warmup;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class RepeatedString {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+
+
+        char[] inputString = s.toCharArray();
+
+        int itr = 0;
+
+        int singleCount = 0;
+
+        long totalCount = 0;
+
+        if (inputString.length == 1 && inputString[0] == 'a') {
+            return n;
+        }
+
+
+        while (itr < inputString.length){
+
+            if (inputString[itr] == 'a') {
+
+                singleCount++;
+            }
+
+            itr = itr + 1;
+
+        }
+
+        totalCount = (n /inputString.length) * singleCount;
+
+        itr = 0;
+
+
+        while (itr < n%inputString.length){
+
+            if (inputString[itr] == 'a') {
+
+                totalCount++;
+            }
+
+            itr = itr + 1;
+
+        }
+
+        return totalCount;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = "aaabc";
+
+//        long n = scanner.nextLong();
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, 10);
+
+
+        System.out.println(result);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+//        scanner.close();
+    }
 }
