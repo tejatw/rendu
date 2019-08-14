0092d68600566c1df250e55f82494ef637485571
lo13 <lo13@global.tesco.org>
2019-08-14 14:28:06 +0530

    cassandra token range query

---
+++ src/cassandra/TestToken.java
@@ -0,0 +1,44 @@
+package cassandra;
+
+import java.math.BigDecimal;
+
+public class TestToken {
+
+    public static void main(String... args) {
+
+        long startToken = -9223372036854775808L;
+
+        long incrementer = 273101875948331383L;
+
+        long endToken = startToken + incrementer;
+
+        int iterator = 1;
+
+//        System.out.println(Math.abs(startToken));
+//        System.out.println(Math.abs(incrementer));
+//        System.out.println(Math.abs(endToken));
+
+
+
+//        while (endToken < 9223372036854775807L && ((Math.abs(startToken) > incrementer || startToken == Long.MIN_VALUE) && Math.abs(endToken) > incrementer)) {
+            while (!(startToken > 0 && endToken < 0)) {
+
+
+//                System.out.println("Iterator is " + iterator);
+
+//            System.out.println("select count(0) from  costamendment  where token(costamendment_uuid) >= " + startToken + " and token(costamendment_uuid) < " + endToken + ";");
+                System.out.println("select count(0) from  product_audit  where token(promotion_id, product_zone_uuid) >= " + startToken + " and token(promotion_id, product_zone_uuid) < " + endToken + ";");
+
+
+                startToken = startToken + incrementer;
+            endToken = startToken + incrementer;
+            iterator += 1;
+
+//            System.out.println(startToken);
+//            System.out.println(incrementer);
+//            System.out.println(endToken);
+        }
+
+        System.out.println("select count(0) from  product_audit  where token(promotion_id, product_zone_uuid) >= " + startToken + " and token(promotion_id, product_zone_uuid) < " + Long.MAX_VALUE + ";");
+    }
+}
